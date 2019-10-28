677bc36354f4d077a8b7748970a92a1302ab8ee0
mumin irican <mumin.irican@siemens.com>
2019-10-28 19:12:59 +0300

    a new file with a syntax error for learning about tuples.

---
+++ 04/demos/demos/after/tuples.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Learn how to use tuples
+
+Usage:
+
+    python3 tuples.py
+"""
+# tuples are defined with (), are immutable
+# t is the global scoped object for this module
+t = ("Norway", 4.953, 3)
+
+
+def loop(tpl):
+    for item in tpl:
+        print(item)
+    print("length of the tuple is ", len(tpl))
+
+
+print(__name__)
+
+
+def main():
+    loop(t)
+
+
+if __name__ == "__main":
+    main()
+else:
+    print("not called properly")
+
